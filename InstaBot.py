a8a45217424cfee2af955a870ce2d2bee70be7fa
Nehal Mahida <nehalmahida2144@gmail.com>
2019-12-29 16:27:36 +0530

    Login and Following a single user is complete.

--- InstaBot.py
+++ InstaBot.py
@@ -1,1 +1,35 @@
-print('Hello from instaBot')
+from selenium import webdriver
+import os
+import time
+
+
+class IgBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.base_url = 'https://www.instagram.com'
+        # you need to download chrome driver according to your chrome browser version.
+        self.driver = webdriver.Chrome("./chromedriver.exe")
+        self.login()
+
+    def login(self):
+        self.driver.get('{}/accounts/login/'.format(self.base_url))
+        time.sleep(2)
+        self.driver.find_element_by_name('username').send_keys(self.username)
+        self.driver.find_element_by_name('password').send_keys(self.password)
+        self.driver.find_elements_by_xpath("//div[contains(text(), 'Log In')]")[0].click()
+        time.sleep(4)
+
+    def nav_user(self, user):
+        self.driver.get('{}/{}'.format(self.base_url, user))
+
+    def follow_user(self, user):
+        self.nav_user(user)
+
+        follow_button = self.driver.find_elements_by_xpath("//button[contains(text(), 'Follow')]")[0].click()
+
+
+if __name__ == '__main__':
+    ig_bot = IgBot('username', 'password')
+    ig_bot.follow_user('xysUser')
+
