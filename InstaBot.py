8059a4ab43490309b7bf86dbcd3e9e463fcfc6f3
Nehal Mahida <nehalmahida2144@gmail.com>
2020-01-01 19:07:43 +0530

    Follow and Unfollow of number of user is completed.

--- InstaBot.py
+++ InstaBot.py
@@ -1,6 +1,11 @@
+from telnetlib import EC
+
 from selenium import webdriver
 import os
 import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class IgBot:
@@ -14,22 +19,53 @@ class IgBot:
 
     def login(self):
         self.driver.get('{}/accounts/login/'.format(self.base_url))
-        time.sleep(2)
+        time.sleep(4)
         self.driver.find_element_by_name('username').send_keys(self.username)
         self.driver.find_element_by_name('password').send_keys(self.password)
         self.driver.find_elements_by_xpath("//div[contains(text(), 'Log In')]")[0].click()
         time.sleep(4)
+
+    def unfollower(self, account):
+        self.driver.get("https://www.instagram.com/{0}/".format(account))
+        time.sleep(2)
+        self.driver.find_element_by_partial_link_text("following").click()
+        time.sleep(4)
+        dialog = self.driver.find_element_by_xpath('/html/body/div[4]/div/div[2]')
+        for i in range(5):
+            self.driver.execute_script("arguments[0].scrollTop = (arguments[0].scrollHeight/5)", dialog)
+            time.sleep(1)
+        path = "/html/body/div[5]/div/div/div[3]/button[1]"  # to click unfollow in pop-up window.
+        for i in range(1, 11):
+            xpath = "/html/body/div[4]/div/div[2]/ul/div/li[{}]/div/div[2]/button".format(i)
+            self.driver.find_elements_by_xpath(xpath)[0].click()
+            self.driver.find_elements_by_xpath(path)[0].click()
+            time.sleep(2)
 
     def nav_user(self, user):
         self.driver.get('{}/{}'.format(self.base_url, user))
 
     def follow_user(self, user):
         self.nav_user(user)
+        self.driver.find_element_by_partial_link_text("followers").click()
+        time.sleep(4)
+        dialog = self.driver.find_element_by_xpath('/html/body/div[4]/div/div[2]')
+        for i in range(5):
+            self.driver.execute_script("arguments[0].scrollTop = (arguments[0].scrollHeight/5)", dialog)
+            time.sleep(1)
 
-        follow_button = self.driver.find_elements_by_xpath("//button[contains(text(), 'Follow')]")[0].click()
+        for i in range(1, 11):
+            xpath = '/html/body/div[4]/div/div[2]/ul/div/li[{}]/div/div[2]'.format(i)
+            self.driver.find_elements_by_xpath(xpath)[0].click()
+            time.sleep(2)
+
+    def unfollow_user(self, user):
+        self.nav_user(user)
+        unfollow_button = self.driver.find_elements_by_xpath("//button[contains(text(), 'Following')]")[0].click()
+        unfollow = self.driver.find_elements_by_xpath("/html/body/div[4]/div/div/div[3]/button[1]")[0].click()
 
 
 if __name__ == '__main__':
     ig_bot = IgBot('username', 'password')
-    ig_bot.follow_user('xysUser')
+    ig_bot.unfollower('the.programeme')
+    ig_bot.follow_user('_codehub_')
 
